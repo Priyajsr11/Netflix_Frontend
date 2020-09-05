9bf6004d35880775f651f3d0c4685e712b46f3b6
Priya Singh <priyajsr65@gmail.com>
2020-09-05 23:18:02 +0530

    Updates

---
+++ precache-manifest.19bde3c94249e9888cfaf4a76ae4ef4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58002787d74f52c3b9d10112eb960d83",
+    "url": "/Netflix_Frontend/index.html"
+  },
+  {
+    "revision": "ce1926d0924c56323a0c",
+    "url": "/Netflix_Frontend/static/css/main.795f2051.chunk.css"
+  },
+  {
+    "revision": "4cf62c0b347721f1b4c9",
+    "url": "/Netflix_Frontend/static/js/2.1812b82c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Netflix_Frontend/static/js/2.1812b82c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce1926d0924c56323a0c",
+    "url": "/Netflix_Frontend/static/js/main.41a7d490.chunk.js"
+  },
+  {
+    "revision": "abdfb2c32b2ecf3bfc2a",
+    "url": "/Netflix_Frontend/static/js/runtime-main.88d100dc.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Netflix_Frontend/precache-manifest.19bde3c94249e9888cfaf4a76ae4ef4b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Netflix_Frontend/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
